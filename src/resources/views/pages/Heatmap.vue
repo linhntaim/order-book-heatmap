749f72bcb5b71e50cb8a0cb91013a45c9146a9b0
Nguyen Tuan Linh <linhntaim@users.noreply.github.com>
2023-07-22 20:43:40 +0700

    + summary

--- src/resources/views/pages/Heatmap.vue
+++ src/resources/views/pages/Heatmap.vue
@@ -1,5 +1,51 @@
 <template lang="pug">
-.chart-container.vw-100.vh-100(ref="chartContainer")
+.chart-wrapper.vw-100.vh-100
+    .legend-container
+        .legend
+            span.legend-inner
+                span.legend-primary.text-nowrap {{ baseSymbol }} / {{ quoteSymbol }} &#183; {{ interval }} &#183; {{ exchange }}
+                span.legend-secondary.legend-group
+                    span.legend-group-item
+                        | O
+                        span(:style="{color: color}") {{ open }}
+                    span.legend-group-item
+                        | H
+                        span(:style="{color: color}") {{ high }}
+                    span.legend-group-item
+                        | L
+                        span(:style="{color: color}") {{ low }}
+                    span.legend-group-item
+                        | C
+                        span(:style="{color: color}") {{ close }}
+        .legend
+            span.legend-inner
+                span.legend-primary Top Ask
+                span.legend-secondary.legend-group(:style="{color: top.asks.color}")
+                    span.legend-group-item {{ top.asks.totalText }}
+                    span.legend-group-item {{ top.asks.quantityText }}
+                    span.legend-group-item {{ top.asks.priceText }}
+        .legend
+            span.legend-inner
+                span.legend-primary Top Bid
+                span.legend-secondary.legend-group(:style="{color: top.bids.color}")
+                    span.legend-group-item {{ top.bids.totalText }}
+                    span.legend-group-item {{ top.bids.quantityText }}
+                    span.legend-group-item {{ top.bids.priceText }}
+        .legend
+            span.legend-inner
+                span.legend-primary Largest Ask
+                span.legend-secondary.legend-group(:style="{color: largest.asks.color}")
+                    span.legend-group-item {{ largest.asks.totalText }}
+                    span.legend-group-item {{ largest.asks.quantityText }}
+                    span.legend-group-item {{ largest.asks.priceText }}
+        .legend
+            span.legend-inner
+                span.legend-primary Largest Bid
+                span.legend-secondary.legend-group(:style="{color: largest.bids.color}")
+                    span.legend-group-item {{ largest.bids.totalText }}
+                    span.legend-group-item {{ largest.bids.quantityText }}
+                    span.legend-group-item {{ largest.bids.priceText }}
+    .chart-container.vw-100.vh-100(ref="chartContainer")
 #reloadModal.modal.fade(tabindex="-1" data-bs-backdrop="static")
     .modal-dialog
         .modal-content
@@ -16,7 +62,7 @@ import {num} from '@/app/support/helpers'
 import {DataHubFactory} from '@/app/support/crypto-services/data-hub-factory'
 import formfactor from 'platform-detect/formfactor.mjs'
 
-let chart, priceSeries, dataHub, heatBookSeries = []
+let chart, priceSeries, dataHub, heatBookSeries = {asks: {}, bids: {}}
 
 export default {
     // eslint-disable-next-line
@@ -28,15 +74,47 @@ export default {
             },
 
             type: 'Candlestick',
-            exchange: this.$route.params.exchange,
+            exchange: this.$route.params.exchange.toUpperCase(),
             baseSymbol: this.$route.params.baseSymbol.toUpperCase(),
             quoteSymbol: this.$route.params.quoteSymbol.toUpperCase(),
             interval: this.$route.params.interval,
 
-            orderBook: {
-                asks: {},
-                bids: {},
+            open: '0',
+            low: '0',
+            high: '0',
+            close: '0',
+            color: '#26a69a',
+
+            top: {
+                asks: {
+                    priceText: '0',
+                    color: '#ffffff',
+                    totalText: '0',
+                    quantityText: '0',
+                },
+                bids: {
+                    priceText: '0',
+                    color: '#ffffff',
+                    totalText: '0',
+                    quantityText: '0',
+                },
             },
+            largest: {
+                asks: {
+                    priceText: '0',
+                    color: '#ffffff',
+                    totalText: '0',
+                    quantityText: '0',
+                },
+                bids: {
+                    priceText: '0',
+                    color: '#ffffff',
+                    totalText: '0',
+                    quantityText: '0',
+                },
+            },
+
+            crosshairMovingInTime: false,
         }
     },
     head() {
@@ -94,7 +172,9 @@ export default {
             chart = createChart(this.$refs.chartContainer, {
                 autoSize: true,
                 layout: {
-                    background: 'rgb(20, 24, 35)',
+                    background: {
+                        color: 'rgb(21, 25, 36)',
+                    },
                     textColor: 'rgb(172, 175, 184)',
                 },
                 grid: {
@@ -118,12 +198,42 @@ export default {
                     rightOffset: 20,
                 },
             })
+            chart.subscribeCrosshairMove(this.listenToCrosshairMove)
+        },
+        listenToCrosshairMove(param) {
+            if (param.time && param.time <= dataHub.latestCandle.candle.time) {
+                this.crosshairMovingInTime = true
+                this.updateCandleOnLegend(param.seriesData.get(priceSeries))
+            }
+            else {
+                this.crosshairMovingInTime = false
+                this.updateLatestCandleToLegend()
+            }
         },
         removeChart() {
             if (chart) {
+                chart.unsubscribeCrosshairMove(this.listenToCrosshairMove)
                 chart.remove()
                 chart = null
             }
+        },
+        updateCandleOnLegend(candle) {
+            if (candle) {
+                const precision = num.precision(dataHub.tickerSize)
+                this.open = candle.open.toFixed(precision)
+                this.high = candle.high.toFixed(precision)
+                this.low = candle.low.toFixed(precision)
+                this.close = candle.close.toFixed(precision)
+                this.color = candle.close >= candle.open ? '#26a69a' : '#ef5350'
+                return true
+            }
+            return false
+        },
+        updateLatestCandleToLegend() {
+            if (!this.crosshairMovingInTime) {
+                return this.updateCandleOnLegend(dataHub?.latestCandle.candle)
+            }
+            return false
         },
         createSeries() {
             const numFormatter = new Intl.NumberFormat('en-US', {
@@ -136,6 +246,8 @@ export default {
             dataHub.on(
                 startingCandles => {
                     updateTitle()
+                    this.updateLatestCandleToLegend()
+
                     priceSeries = chart.addCandlestickSeries({
                         priceFormat: {
                             precision: num.precision(dataHub.tickerSize),
@@ -151,34 +263,108 @@ export default {
                 },
                 updatingCandle => {
                     updateTitle()
+                    this.updateLatestCandleToLegend()
+
                     priceSeries.update(updatingCandle)
                 },
                 heatBook => {
-                    this.removeHeatBookSeries()
-                    this.createHeatSideSeries(heatBook.asks)
-                    this.createHeatSideSeries(heatBook.bids)
+                    this.createHeatSideSeries(heatBook.asks, 'asks')
+                    this.createHeatSideSeries(heatBook.bids, 'bids')
                 },
             )
         },
-        createHeatSideSeries(heatSideCollection) {
+        createHeatSideSeries(heatSideCollection, side = 'bids') {
+            const oldHeatPrices = Object.keys(heatBookSeries[side])
+
+            const topHeat = {
+                price: -1,
+
+                priceText: '0',
+                color: '#ffffff',
+                totalText: '0',
+                quantityText: '0',
+            }
+            const largestHeat = {
+                total: 0,
+
+                priceText: '0',
+                color: '#ffffff',
+                totalText: '0',
+                quantityText: '0',
+            }
             Object.keys(heatSideCollection).forEach(
                 heatPrice => {
                     const heatSideItem = heatSideCollection[heatPrice]
-                    const heatSeries = chart.addCandlestickSeries({
-                        priceLineVisible: false,
-                        lastValueVisible: false,
-                    })
-                    heatSeries.setData(heatSideItem.candles)
-                    heatSeries.setMarkers(heatSideItem.markers)
-                    heatBookSeries.push(heatSeries)
+
+                    const price = heatSideItem.data[0].price
+                    if (topHeat.price === -1
+                        || ((side === 'asks' && price < topHeat.price)
+                            || (side === 'bids' && price > topHeat.price))
+                    ) {
+                        topHeat.price = price
+
+                        topHeat.priceText = heatSideItem.data[0].priceText
+                        topHeat.color = heatSideItem.data[0].color
+                        topHeat.totalText = heatSideItem.data[0].totalText
+                        topHeat.quantityText = heatSideItem.data[0].quantityText
+                    }
+                    const total = heatSideItem.data[0].total
+                    if (total > largestHeat.total) {
+                        largestHeat.total = total
+
+                        largestHeat.priceText = heatSideItem.data[0].priceText
+                        largestHeat.color = heatSideItem.data[0].color
+                        largestHeat.totalText = heatSideItem.data[0].totalText
+                        largestHeat.quantityText = heatSideItem.data[0].quantityText
+                    }
+
+                    const found = oldHeatPrices.indexOf(heatPrice)
+                    if (found === -1) { // not found - create new
+                        const heatSeries = chart.addCandlestickSeries({
+                            priceLineVisible: false,
+                            lastValueVisible: false,
+                        })
+                        heatSeries.setData(heatSideItem.candles)
+                        heatSeries.setMarkers(heatSideItem.markers)
+                        heatBookSeries[side][heatPrice] = heatSeries
+                    }
+                    else { // found - update
+                        const heatSeries = heatBookSeries[side][heatPrice]
+                        heatSeries.setData(heatSideItem.candles)
+                        heatSeries.setMarkers(heatSideItem.markers)
+                        heatSeries.applyOptions({
+                            visible: true,
+                        })
+
+                        oldHeatPrices.splice(found, 1)
+                    }
                 },
             )
+            this.top[side].priceText = topHeat.priceText
+            this.top[side].color = topHeat.color
+            this.top[side].totalText = topHeat.totalText
+            this.top[side].quantityText = topHeat.quantityText
+            this.largest[side].priceText = largestHeat.priceText
+            this.largest[side].color = largestHeat.color
+            this.largest[side].totalText = largestHeat.totalText
+            this.largest[side].quantityText = largestHeat.quantityText
+
+            // hide old which not updated
+            oldHeatPrices.forEach(heatPrice => {
+                heatBookSeries[side][heatPrice].applyOptions({
+                    visible: false,
+                })
+            })
+        },
+        removeHeatSideSeries(heatSideSeries) {
+            Object.keys(heatSideSeries).forEach(heatPrice => {
+                chart.removeSeries(heatSideSeries[heatPrice])
+                delete heatBookSeries[heatPrice]
+            })
         },
         removeHeatBookSeries() {
-            heatBookSeries.forEach(heatSeries => {
-                chart.removeSeries(heatSeries)
-            })
-            heatBookSeries = []
+            this.removeHeatSideSeries(heatBookSeries.asks)
+            this.removeHeatSideSeries(heatBookSeries.bids)
         },
         removeSeries() {
             if (priceSeries) {
@@ -192,7 +378,69 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.chart-wrapper {
+    background: rgb(21, 25, 36);
+    color: rgb(172, 175, 184);
+}
+
 .chart-container {
-    background: rgb(22, 26, 30);
+    cursor: crosshair;
+}
+
+.legend-container {
+    position: absolute;
+    z-index: 3;
+    pointer-events: none;
+    user-select: none;
+    color: #ecececff;
+    padding: .5rem;
+}
+
+.legend {
+    &:not(:first-child) {
+        margin-top: .5rem;
+        font-size: .85rem;
+
+        .legend-secondary {
+            font-size: .85rem;
+        }
+    }
+}
+
+.legend-inner {
+    display: inline-block;
+    cursor: default;
+    pointer-events: auto;
+    background: rgba(19, 23, 34, .7);
+}
+
+.legend-primary {
+    display: inline-block;
+    padding: 0 .3rem;
+    border: 1px solid transparent;
+    border-radius: .375rem;
+
+    &:hover {
+        background: rgb(19, 23, 34);
+        border-color: rgb(54, 58, 69);
+    }
+}
+
+.legend-secondary {
+    display: inline-block;
+    padding: 0 .3rem;
+    font-size: .875rem;
+}
+
+.legend-group-item {
+    &:not(:first-child) {
+        margin-left: .3rem;
+    }
+}
+
+@media (max-width: 767px) {
+    .legend-secondary {
+        display: block;
+    }
 }
 </style>
